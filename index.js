e447d3438165337e95ced619fb9fe68014206944
Gustavo Marcello <gustavo.marcello@compasso.com.br>
2022-01-09 15:11:30 -0300

    v1

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const { text } = require("body-parser");
-
 const { RasaNodeActionServer, RasaActionEvent } = require("rasa-node-action-server");
 const rnas = new RasaNodeActionServer();
 
+// importação de cada action criada na pasta node_actions
 const welcome = require('./node_actions/welcome');
 const check = require('./node_actions/check');
 
+// instanciando as actions
 welcome.welcomeNode_action.welcome_node(rnas, RasaActionEvent);
 check.checkNode_action.check_node(rnas, RasaActionEvent);
 
+// rodando o servidor
 rnas.start();
